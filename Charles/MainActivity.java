14d8013b6c5e2aaf9d14de1d846cd888b204f128
Charles Mo <72364282+chmo2019@users.noreply.github.com>
2020-11-25 20:38:18 -0500

    Add files via upload
    Game object - Abstract class defining object with update and draw methods
    RectPlayer - defines player movements according to user's finger
    Constants - configuration file storing maximum height and width parameters
    MainActivity - main activity
    MainThread - defines main thread
    Obstacle - defines vertically moving obstacle
    Game panel - main game loop
    Obstacle Manager - populates obstacles

---
+++ Charles/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.myapplication;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.view.Window;
+import android.view.WindowManager;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+
+        DisplayMetrics dm = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getRealMetrics(dm);
+        Constants.SCREEN_WIDTH = dm.widthPixels;
+        Constants.SCREEN_HEIGHT = dm.heightPixels;
+
+        setContentView(new GamePanel(this));
+    }
+}
