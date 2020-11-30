57d75e31ed17318cdb3ed96b55b3c03838ed43b9
Alperen Kitapçı <alperenmirac@gmail.com>
2020-11-29 21:23:30 -0500

    added Ben's project

---
+++ Asteroids_Squared/app/src/main/java/com/ec327/asteroids/SpaceShip.java
@@ -0,0 +1,38 @@
+package com.ec327.asteroids;
+
+import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+
+public class SpaceShip
+{
+    private double x,y;
+    // to determine how fast we want the ship to move;
+    private final int velocity = 10;
+    //we want ship to start in middle.
+    private final int screenWidth = Resources.getSystem().getDisplayMetrics().widthPixels;
+    private final int screenHeight = Resources.getSystem().getDisplayMetrics().heightPixels;
+        private Bitmap ship;
+
+        public SpaceShip(Bitmap ship) {
+            this.ship = ship;
+            //have to set middle fo screen
+            x=screenWidth/2;
+            y=screenHeight/2;
+        }
+
+        public void draw(Canvas canvas) {
+            canvas.drawBitmap(ship, (int)x, (int)y, null);
+        }
+        public void update(JoyStick j)
+        {
+           x+=(velocity*j.getPositionX());
+           y+=(velocity*j.getPositionY());
+
+        }
+
+    public void setPosition(float x,float y) {
+            this.x=x;
+            this.y=y;
+    }
+}
