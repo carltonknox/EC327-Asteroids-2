c4a398edfd160453599e38849782f62d8d6404ba
Benjamin <60583572+benmarincanseco@users.noreply.github.com>
2020-12-06 17:11:12 -0500

    Update SpaceShip.java

--- app/src/main/java/com/example/asteroids_engine_test/SpaceShip.java
+++ app/src/main/java/com/example/asteroids_engine_test/SpaceShip.java
@@ -9,30 +9,66 @@ public class SpaceShip extends SpaceObject
     // to determine how fast we want the ship to move;
     private final int velocity = 30;
     //we want ship to start in middle.
-    private final static int screenWidth = Resources.getSystem().getDisplayMetrics().widthPixels;
-    private final static int screenHeight = Resources.getSystem().getDisplayMetrics().heightPixels;
-    private Bitmap ship;
+    private final int screenWidth = Resources.getSystem().getDisplayMetrics().widthPixels;
+    private final int screenHeight = Resources.getSystem().getDisplayMetrics().heightPixels;
+    private Bitmap ship1;
+    private Bitmap ship2;
+    private Bitmap finalShip;
+    private float degrees;//rotation degree
+    private Matrix matrix = new Matrix();
 
-    public SpaceShip(Bitmap ship) {
-        super(screenWidth/2,screenHeight/2,0,0);
-        this.ship = ship;
-        //have to set middle fo screen
-
-    }
-
-    public void draw(Canvas canvas) {
-        canvas.drawBitmap(ship, x, y, null);
-    }
-    public void update(JoyStick j)
+    public SpaceShip(Bitmap shipon,Bitmap shipoff) 
     {
-        this.dx= (int) (velocity*j.getPositionX());
-        this.dy= (int) (velocity*j.getPositionY());
-        this.x = bind((this.x+this.dx),xMax);
-        this.y = bind((this.y+this.dy),yMax);
-    }
+           //scales my ships to an appropriate size;
+       Bitmap example = Bitmap.createScaledBitmap(shipon,180,180,true);
+       Bitmap example2 = Bitmap.createScaledBitmap(shipoff,180,180,true);
+       this.ship1 = example;
+       this.ship2 = example2;
+
+            //have to set middle fo screen
+            x=screenWidth/2;
+            y=screenHeight/2;
+        }
+
+        public void draw(Canvas canvas)
+        {
+            int imgx = (int)x-finalShip.getWidth()/2;
+            int imgy = (int)y-finalShip.getHeight()/2;
+
+            canvas.drawBitmap(finalShip, (int)imgx, (int)imgy, null);
+            canvas.drawBitmap(finalShip, (int)imgx+screenWidth, (int)imgy, null);
+            canvas.drawBitmap(finalShip, (int)imgx-screenWidth, (int)imgy, null);
+            canvas.drawBitmap(finalShip, (int)imgx, (int)imgy+screenHeight, null);
+            canvas.drawBitmap(finalShip, (int)imgx, (int)imgy-screenHeight, null);
+        }
+        
+        public void update(JoyStick j)
+        {
+            degrees=j.getDegrees();
+            matrix.setRotate(degrees);
+            if(j.getPositionY()==0&&j.getPositionX()==0)
+            {
+                finalShip = Bitmap.createBitmap(ship2, 0, 0, ship1.getWidth(), ship1.getHeight(), matrix, true);
+            }
+            else
+            {
+                finalShip = Bitmap.createBitmap(ship1, 0, 0, ship1.getWidth(), ship1.getHeight(), matrix, true);
+            }
+
+
+
+           x = bind((x+(velocity*j.getPositionX())),screenWidth);
+           y = bind((y+(velocity*j.getPositionY())),screenHeight);
+
+        }
 
     public void setPosition(int x,int y) {
         this.x=x;
         this.y=y;
     }
+    public float getA()
+    {
+        return (float) degrees;
+    }
+    
 }
